5bcf17a076383de2116971cdcde8e079c812fac2
emerson2h <emershito_25@hotmail.com>
2017-07-17 19:41:29 -0500

    carrito de compras

---
+++ CuyStore/Controllers/CompraController.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace CuyStore.Controllers
+{
+    public class CompraController : Controller
+    {
+        // GET: Compra
+        public ActionResult Paso1()
+        {
+            return View();
+        }
+        public ActionResult Paso2()
+        {
+            return View();
+        }
+        public ActionResult Paso3()
+        {
+            return View();
+        }
+        public ActionResult Paso4()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public JsonResult RealizarPedido(List<Pedido> p)
+        {
+
+            return Json(true, JsonRequestBehavior.AllowGet);
+        }
+
+        public class Pedido
+        {
+            public int ProductoId { get; set; }
+            public string Denominacion { get; set; }
+            public int Cantidad { get; set; }
+            public string Imagen { get; set; }
+            public decimal PrecioUnitario { get; set; }
+        }
+
+    }
+}
